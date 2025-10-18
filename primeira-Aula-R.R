875414244866088328727d3d56d2a74df09c3c48
JoaoAdelson <joaoadelsondejesus@gmail.com>
2025-10-17 21:24:29 -0300

    Add files via upload

---
+++ primeira-Aula-R.R
@@ -0,0 +1,23 @@
+1 + 1
+soma_1 <- 1 + 1
+
+3 + 3
+soma_2 <- 3 + 3
+
+lista_frutas <- c("maçâ", "banana", "melão")
+
+A <- c(1, 2, 3, 4)
+A
+z <- matrix(c(7, 54, 14, 35), nrow = 2, ncol = 2, byrow = TRUE)
+z
+
+x <- matrix(c(7, 54,-1, 14, 35, -10), nrow = 2, ncol = 3, byrow = TRUE)
+x
+y <- matrix(c(7, 54,-1, 14, 35, -10), nrow = 3, ncol = 2, byrow = TRUE)
+y
+
+R <- matrix(c(2, -5, -8, 3, 4, 0), nrow = 2, ncol = 3, byrow = TRUE)
+S <- matrix(c(1, 0, 5, 6, -1, -7), nrow = 2, ncol = 3, byrow = TRUE)
+
+soma_R_S <- R+S
+soma_R_S
